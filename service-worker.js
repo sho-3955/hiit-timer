dbf2981591941726506144dcea56c73255b0d801
sho-3955 <ksho55007@gmail.com>
2025-04-29 16:47:26 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,73 @@
+// サービスワーカーのバージョン
+const CACHE_NAME = 'hiit-timer-v1';
+
+// キャッシュするファイルリスト
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-167x167.png',
+  './icons/icon-180x180.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png'
+];
+
+// サービスワーカーのインストール時にキャッシュを作成
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('キャッシュを開きました');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// アクティベート時に古いキャッシュを削除
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// フェッチイベントの処理（ネットワークファースト戦略）
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // 有効なレスポンスを受け取ったらキャッシュに保存
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        
+        const responseToCache = response.clone();
+        
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+          
+        return response;
+      })
+      .catch(() => {
+        // ネットワークが使えない場合はキャッシュから取得
+        return caches.match(event.request);
+      })
+  );
+}); 
